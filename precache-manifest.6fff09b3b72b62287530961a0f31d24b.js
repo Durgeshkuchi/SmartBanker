fc4dc09ca6ea8bbb28126e85a75eccde4ba9cde5
Durgesh Kuchi <durgesh@joinvulse.com>
2025-07-03 17:38:22 -0500

    Updates

---
+++ precache-manifest.6fff09b3b72b62287530961a0f31d24b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4daf66515675fecc38dafd47177b503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ebf4ccfb8546cf3fbc",
+    "url": "/static/css/main.e534204b.chunk.css"
+  },
+  {
+    "revision": "30813cb661e2badc7770",
+    "url": "/static/js/2.a3a0bf87.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/static/js/2.a3a0bf87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ebf4ccfb8546cf3fbc",
+    "url": "/static/js/main.a2d498ec.chunk.js"
+  },
+  {
+    "revision": "22f49a50020bac1a337b",
+    "url": "/static/js/runtime-main.f3a46c76.js"
+  },
+  {
+    "revision": "09417f976b72dbf176d5f91479f17119",
+    "url": "/static/media/Login page.09417f97.png"
+  },
+  {
+    "revision": "29be58c182fa1b25c2dcd0c80f02658f",
+    "url": "/static/media/smartBankerIcon.29be58c1.png"
+  },
+  {
+    "revision": "fa73afda7826ce098be3e4863e2d2552",
+    "url": "/static/media/user.fa73afda.png"
+  }
+]);
